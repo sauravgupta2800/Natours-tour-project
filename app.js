2928c4c55365aef0565496bde4ee03199a2523ee
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-04-15 10:47:12 +0530

    environment variabled added

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+
 const morgan = require('morgan');
+
 const tourRouter = require('./routes/tourRoutes');
+
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 // 1. MIDDLEWARE
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); // helps render public folder, diff than router
 app.use((req, res, next) => {
-  console.log('Hello from middleware');
+  // console.log('Hello from middleware');
   next();
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' }); // such that we will be able to access
+
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('app is listening at post ' + port);
+  // console.log('app is listening at post ' + port);
 });
