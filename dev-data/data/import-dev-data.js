6962b84fb9855319fb922a8a22d1d6a033d8e503
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-04-17 18:15:37 +0530

    written scripts  for bulk delete & bulk import file to DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: `../../config.env` }); // such that we will be able to access
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connection successfull 2 !'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded !');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted !');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
