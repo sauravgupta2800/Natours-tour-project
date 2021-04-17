74f5bb2772bf65c9e5e02dbe8b9a23a3a95b82f9
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-04-17 11:46:57 +0530

    set the remote host in the project

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' }); // such that we will be able to access
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('db connection successfull !'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
