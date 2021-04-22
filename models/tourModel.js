adfe3e1083b7335bf92ef6e31cbe1e1e2657d01d
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-04-22 16:59:20 +0530

    priceDiscount custom validator added

--- models/tourModel.js
+++ models/tourModel.js
@@ -48,7 +48,15 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+      },
+      message: 'Discount price should be below the regular price',
+    },
     summary: {
       type: String,
       trim: true,
